da6c40510c6bd1e070fc0e699fb83ace7b6924ce
Ash15157 <64275598+Ash15157@users.noreply.github.com>
2021-10-01 14:27:22 -0400

    added javascript stuffh to do audio

--- shuba.js
+++ shuba.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
                 const audio = document.querySelector("audio");
                 audio.volume = 0.8;
                 audio.play();
+  
+  $( "#volume" ).on( "click", function() {
+   if(audio.paused) {
+     audio.play();
+   } else {
+     audio.pause();
+   }
+});
 });
