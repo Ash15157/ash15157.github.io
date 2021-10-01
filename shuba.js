30cecc12cedbb738aac5a66383dfa3c6e1c0b610
Ash15157 <64275598+Ash15157@users.noreply.github.com>
2021-10-01 14:05:24 -0400

    less piss pants

--- shuba.js
+++ shuba.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-
-window.addEventListener("DOMContentLoaded", event => {
                 const audio = document.querySelector("audio");
                 audio.volume = 0.8;
                 audio.play();
-            });
 });
