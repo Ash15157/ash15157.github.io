a3b3b2cb5a246dba2de052fdea604dd680c1f9e8
Ash15157 <64275598+Ash15157@users.noreply.github.com>
2022-02-05 21:30:22 -0500

    Added volume slider and pet duck feature along with other bug fixes to responsive design

--- shuba.js
+++ shuba.js
@@ -1,14 +1,19 @@
+var audio;
 $(document).ready(function() {
-                const audio = document.querySelector("#song");
-                audio.volume = 0.5;
+                audio = document.querySelector("#song");
+               // audio.volume = 0.5;
                 const noise = document.querySelector("#noise");
-                noise.volume = 0.5;
+                // noise.volume = 0.5;
   
   $( "#volume" ).on( "click", function() {
    if(audio.paused) {
      audio.play();
+     audio.volume = 0.50;
+     $("#volumeSlider").val(50);
    } else {
      audio.pause();
+     audio.volume = 0.0;
+     $("#volumeSlider").val(0.0);
    }
     $("#icon").toggleClass("bi-volume-mute").toggleClass("bi-volume-up");
 });
@@ -20,4 +25,28 @@ $(document).ready(function() {
      noise.pause();
    }
   });
+
+  $("#petDuckButton").on("click",function(){
+      if(noise.paused) {
+          noise.play();
+      } else {
+          noise.pause();
+      }
+  })
+
+
 });
+
+function volumeChange(val){
+    let audioVolume = val/100;
+    audio.volume = audioVolume;
+    if(audioVolume === 0.0){
+        audio.pause();
+        $("#icon").removeClass("bi-volume-up");
+        $("#icon").addClass("bi-volume-mute");
+    } else {
+        audio.play();
+        $("#icon").removeClass("bi-volume-mute");
+        $("#icon").addClass("bi-volume-up");
+    }
+}
